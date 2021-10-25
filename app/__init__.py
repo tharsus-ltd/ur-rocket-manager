6e19d7a521391c9e36b394e6d3cdd22bf8cf6f70
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2021-10-25 10:12:22 +0100

    basic tracing

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ __startup_time__ = int(os.environ.get("STARTUP_TIME", "10"))
 USER_SECRET = os.environ.get("SECRET_KEY", "e9629f658c37859ab9d74680a3480b99265c7d4c89224280cb44a255c320661f")
 USER_URL = os.environ.get("USER_URL", "http://user_manager/token")
 
+JAEGER_HOST = os.environ.get("JAEGER_HOST", "jaeger")
+JAEGER_PORT = os.environ.get("JAEGER_PORT", "5775")
+
 # Rocket specific config
 MIN_ENGINES = int(os.environ.get("MIN_ENGINES", "1"))
 MAX_ENGINES = int(os.environ.get("MAX_ENGINES", "8"))
